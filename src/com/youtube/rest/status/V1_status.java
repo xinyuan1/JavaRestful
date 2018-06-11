6a310b5a148061c8bc1c6991c16b2b563d6f06c8
xinyuan1 <zhiguow12@gmail.com>
2018-06-11 11:27:15 -0500

    REST

---
+++ src/com/youtube/rest/status/V1_status.java
@@ -0,0 +1,27 @@
+package com.youtube.rest.status;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
+
+@Path("/v1/status")
+public class V1_status {
+	
+	private static final String api_version = "00.01.00";
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle(){
+		return "<p>Java Web Service<p>";		
+		
+	}
+
+	@Path("/version")
+	@GET		
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>Version: <p>"+api_version+" "+ new java.util.Date();		
+		
+	}
+
+}
